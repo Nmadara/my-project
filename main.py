eb20f1aca098da2886f321d8cb3584214bc55e85
Nmadara <rikudo835@gmail.com>
2024-11-06 14:48:09 +0200

    add initial html css and py file

---
+++ main.py
@@ -0,0 +1,7 @@
+# def print_message(username, message):
+#     return f"Message from {username}: /n {message}"
+#
+# print(print_message('selena', 'hi i m pdi'))
+integer = 32
+integer += 3, 2, 6
+print(integer)pwd
